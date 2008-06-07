7160041e20a7a816dd9297b918e8c2fcb5c3ced8
David South <davidsouth@gmail.com>
2008-06-07 14:00:56 -0600

    Updated uploaded_data= method in attachment_fu_hacks to match new version in attachment_fu. Also updated sanitize_filename method to utilize better non-alphanumeric matcher.

--- vendor/plugins/attachment_fu_hacks/lib/attachment_fu_hacks.rb
+++ vendor/plugins/attachment_fu_hacks/lib/attachment_fu_hacks.rb
@@ -3,17 +3,24 @@ Technoweenie::AttachmentFu::InstanceMethods.module_eval do
   # Overriding this method to allow content_type to be detected when
   # swfupload submits images with content_type set to 'application/octet-stream'
   def uploaded_data=(file_data)
-    return nil if file_data.nil? || file_data.size == 0
-    self.content_type = detect_mimetype(file_data)
-    self.filename     = file_data.original_filename if respond_to?(:filename)
+    if file_data.respond_to?(:content_type)
+      return nil if file_data.size == 0
+      self.content_type = detect_mimetype(file_data)
+      self.filename     = file_data.original_filename if respond_to?(:filename)
+    else
+      return nil if file_data.blank? || file_data['size'] == 0
+      self.content_type = file_data['content_type']
+      self.filename =  file_data['filename']
+      file_data = file_data['tempfile']
+    end
     if file_data.is_a?(StringIO)
       file_data.rewind
       self.temp_data = file_data.read
     else
-      self.temp_path = file_data.path
+      self.temp_path = file_data
     end
   end
-  
+
   def detect_mimetype(file_data)
     if file_data.content_type.strip == "application/octet-stream"
       return File.mime_type?(file_data.original_filename)
@@ -32,7 +39,7 @@ Technoweenie::AttachmentFu::InstanceMethods.module_eval do
       name.gsub! /^.*(\\|\/)/, ''
     
       # Finally, replace all non alphanumeric, underscore or periods with underscore
-      name.gsub! /[^\w\.\-]/, '_'
+      name.gsub! /[^A-Za-z0-9\.\-]/, '_'
     
       # Remove multiple underscores
       name.gsub!(/\_+/, '_')
